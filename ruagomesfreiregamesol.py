8218b52923c33b5def247134b1d213de26cfd9a0
Velhinho <goncalo.velhinho420@gmail.com>
2019-10-13 17:19:36 +0100

    Add comments to SearchProblem class

--- ruagomesfreiregamesol.py
+++ ruagomesfreiregamesol.py
@@ -3,16 +3,16 @@ import pickle
 import time
 
 
+
 class SearchProblem:
 
   def __init__(self, goal, model, auxheur = []):
-    self.goal = goal
-	self.model = model
-	self.auxheur = auxheur 
+    self.goal = goal  # Goal position
+    self.model = model  # Adjacency list of the graph
+    self.auxheur = auxheur  # Map coordenates of each node
     return
 
   def search(self, init, limitexp = 2000, limitdepth = 10, tickets = [math.inf,math.inf,math.inf]):
-    ##
-    ## to implement
-    ##
+    # init = initial position
+    
     return []
