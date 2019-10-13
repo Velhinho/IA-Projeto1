5230d2d7c480d35f0a66cca48d18ce963c008a74
Velhinho <goncalo.velhinho420@gmail.com>
2019-10-13 16:25:23 +0100

    Initial commit

---
+++ ruagomesfreiregamesol.py
@@ -0,0 +1,18 @@
+import math
+import pickle
+import time
+
+
+class SearchProblem:
+
+  def __init__(self, goal, model, auxheur = []):
+    self.goal = goal
+	self.model = model
+	self.auxheur = auxheur 
+    return
+
+  def search(self, init, limitexp = 2000, limitdepth = 10, tickets = [math.inf,math.inf,math.inf]):
+    ##
+    ## to implement
+    ##
+    return []
