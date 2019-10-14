bbfadda7a6d9f4cb6e87eeca87c076b625391f98
Velhinho <goncalo.velhinho420@gmail.com>
2019-10-14 13:07:06 +0100

    BFS works!!!!!

--- ruagomesfreiregamesol.py
+++ ruagomesfreiregamesol.py
@@ -4,12 +4,13 @@ import time
 
 
 class Node:
-  def __init__(self, transport_list):
+  def __init__(self, transport_list, position):
     self.state = "undiscovered"
-    self.parent = -1
+    self.parent = None
     self.adjacency_list = self.add_adj_list(transport_list)
     self.transport_list = transport_list
-  
+    self.position = position
+    
   def add_adj_list(self, transport_list):
     adjacency_list = []
     for transport_tuple in transport_list:
@@ -35,6 +36,9 @@ class Node:
   def get_transport_list(self):
     return self.transport_list
 
+  def get_position(self):
+    return self.position
+
 
 class SearchProblem:
 
@@ -52,14 +56,54 @@ class SearchProblem:
     # init = initial position
 
     graph = self.make_graph()
+    start_node = graph[init[0]]
+    goal_node = graph[self.goal[0]]
+    result = self.bfs(graph, start_node, goal_node)
     
+    asd = []
+    for node in result:
+      print(node.get_position())
+    print(asd)
+
     return []
 
   def make_graph(self):
     graph = []
-    for transport_list in self.model:
-      node = Node(transport_list)
-      graph.append(node)
+    position = 0
     
-    graph.pop(0)  # first node is empty
+    for transport_list in self.model:
+      node = Node(transport_list, position)
+      graph.append(node)
+      position += 1
+    
     return graph
+
+  def bfs(self, graph, start, end):
+    def find_path(graph, start, end):
+
+      if start.get_position() == end.get_position():
+        return [start]
+
+      elif end.get_parent() == None:
+        return []
+
+      else:
+        return find_path(graph, start, end.get_parent()) + [end]
+  
+    start.set_state("discovered")
+    start.set_parent(None)
+
+    queue = []
+    queue.append(start)
+
+    while len(queue) > 0:
+      vertex_a = queue.pop(0)
+      for vertex_b in vertex_a.get_adjacency_list():
+        if graph[vertex_b].get_state() == "undiscovered":
+          graph[vertex_b].set_state("discovered")
+          graph[vertex_b].set_parent(vertex_a)
+          queue.append(graph[vertex_b])
+        
+        vertex_a.set_state("expanded")
+    
+    return find_path(graph, start, end)
